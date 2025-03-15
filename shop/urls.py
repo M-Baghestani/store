0597ed105260692d44389a2cb12ea2b52ff4c484
MohammadHossein Baghestani <mh.baghestani@khu.ac.ir>
2025-03-15 05:19:43 +0330

    Third Commit
    Change API
    example.com/api/....

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,24 @@
 from django.urls import path, include
 from .views import helloworld
+from .views import (
+    CategoryListCreateView, CategoryDetailView,
+    CustomerListCreateView, CustomerDetailView,
+    ProductListCreateView, ProductDetailView,
+    OrderListCreateView, OrderDetailView
+) 
 
 urlpatterns = [
-    path('',helloworld)
+    path('',helloworld),
+
+    path('categories/', CategoryListCreateView.as_view(), name='category-list'),
+    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
+
+    path('customers/', CustomerListCreateView.as_view(), name='customer-list'),
+    path('customers/<int:pk>/', CustomerDetailView.as_view(), name='customer-detail'),
+
+    path('products/', ProductListCreateView.as_view(), name='product-list'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+
+    path('orders/', OrderListCreateView.as_view(), name='order-list'),
+    path('orders/<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,44 @@
 from django.shortcuts import render, HttpResponse
-from .models import Product
-
+from rest_framework import generics
+from .models import Category, Customer, Product, Order
+from .serializers import CategorySerializer, CustomerSerializer, ProductSerializer, OrderSerializer
 
 def helloworld(request):
     all_products = Product.objects.all()
     return render(request, 'index.html', {"products": all_products})
+
+# دسته‌بندی‌ها
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+# مشتریان
+class CustomerListCreateView(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class CustomerDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+# محصولات
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# سفارشات
+class OrderListCreateView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
